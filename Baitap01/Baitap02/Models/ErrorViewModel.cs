806699543276ad8b4574cc32f619239db832b3f3
hphuc28 <dhphuc2806@gmail.com>
2024-08-28 13:31:27 +0700

    baitapvenha

--- Baitap01/Baitap02/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Baitap02.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
