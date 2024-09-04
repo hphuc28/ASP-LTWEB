eae27012ffbb5bec4883180367b9dd08ae8d7441
hphuc28 <dhphuc2806@gmail.com>
2024-09-04 16:40:49 +0700

    e nop bai a

---
+++ BaiTapVeNha02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Do Hoang Phuc";
+            ViewBag.MSSV = "1822030520";
+            ViewBag.Nam = " 2024 ";
+            return View();
+        }
+        public IActionResult MayTinh(int a, int b, string pheptinh)
+        {
+            double result = 0;
+            string message = "";
+
+            try
+            {
+                switch (pheptinh.ToLower())
+                {
+                    case "cộng":
+                        result = a + b;
+                        break;
+                    case "trừ":
+                        result = a - b;
+                        break;
+                    case "nhân":
+                        result = a * b;
+                        break;
+                    case "chia":
+                        if (b != 0)
+                            result = (double)a / b;
+                        else
+                            message = "Không thể chia cho 0.";
+                        break;
+                    default:
+                        message = "Phép tính không hợp lệ.";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                message = $"Lỗi: {ex.Message}";
+            }
+
+            ViewBag.Result = result;
+            ViewBag.Message = message;
+            return View();
+        }
+    }
+}
