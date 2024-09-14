6005cb4908588d3712188817d81fef7f496c1cc5
hphuc28 <dhphuc2806@gmail.com>
2024-09-14 15:12:35 +0700

    em nop bai a

---
+++ BaiTapKiemTra02/Controllers/TaiKhoanController.cs
@@ -0,0 +1,28 @@
+﻿using BaiTapKiemTra02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra02.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string result = $"Tên tài khoản: {model.Username}\n" +
+                                $"Mật khẩu: {model.Password}\n" +
+                                $"Họ tên: {model.HoTen}\n" +
+                                $"Tuổi: {model.Tuoi}";
+                return Content(result, "text/plain");
+            }
+            return View(model);
+        }
+    }
+
+
+
+}
