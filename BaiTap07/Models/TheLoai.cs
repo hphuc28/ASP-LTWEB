3ce1ec26b249ec50f4b803c6bd9056f536b0f3e1
hphuc28 <dhphuc2806@gmail.com>
2024-09-25 12:49:16 +0700

    nop bai

---
+++ BaiTap07/Models/TheLoai.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaiTap07.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Không được để trống!")]
+        [Display(Name="Thể Loại")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Không đúng định dạng ngày tháng năm!")]
+        [Display(Name="Thể Loại")]
+        public DateTime datacreate { get; set; } = DateTime.Now;
+    }
+}
